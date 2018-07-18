39befed94c0b1e3cd105233c4556ddfe91893f56
GiadaSimonetti <21024993+GiadaSimonetti@users.noreply.github.com>
2018-07-18 19:25:12 +0100

    Add logo + style to logo + animation to logo

--- src/App.js
+++ src/App.js
@@ -1,20 +1,26 @@
 import React, { Component } from "react";
-import { TimelineLite } from "gsap";
+import { Power2, TimelineLite } from "gsap";
 import './App.css';
-// import logo from './logo.svg';
+import logo from './logo.svg';
 
 class App extends Component {
   constructor(props){
     super(props);
     this.intro = React.createRef(); // shiny new React 16.3 ref API!
+    this.logo = React.createRef();
   }
 
   componentDidMount() {
       const tl = new TimelineLite();
-
       tl
         .to(this.intro.current, 4.5, { opacity: 1, delay: 1 })
-        .to(this.intro.current, 1.5, { opacity: 0 });
+        .to(this.intro.current, 1.5, { opacity: 0 })
+        .set(this.logo.current, { opacity: 1, scale: 2.75 })
+        .to(this.logo.current, 8, {
+          scale: 0.05,
+          ease: Power2.easeOut
+        })
+        .to(this.logo.current, 1.5, { opacity: 0 }, "-=1.5");
     }
 
   render() {
@@ -23,6 +29,9 @@ class App extends Component {
         <section className="intro" ref={this.intro}>
           <p>A long time ago, in a galaxy far,<br /> far away...
           </p>
+        </section>
+        <section className="logo" ref={this.logo}>
+          <img src={logo} alt="Code Wars logo" />
         </section>
       </div>
     );
