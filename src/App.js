ab764ed20d45294cf5c555f07e59dfd672b15b8d
GiadaSimonetti <21024993+GiadaSimonetti@users.noreply.github.com>
2018-07-18 19:07:46 +0100

    Import gsap + create componentDidMount method

--- src/App.js
+++ src/App.js
@@ -1,12 +1,34 @@
 import React, { Component } from "react";
-import './App.css';
-import logo from './logo.svg';
+import { TweenLite } from "gsap";
+// import './App.css';
+// import logo from './logo.svg';
 
 class App extends Component {
+  constructor(props){
+    super(props);
+    this.intro = React.createRef(); // shiny new React 16.3 ref API!
+  }
+
+  componentDidMount(){
+    TweenLite.to(
+      this.intro.current,
+      4.5,
+      { opacity: 1, delay: 1 }
+    );
+    TweenLite.to(
+      this.intro.current,
+      1.5,
+      { opacity: 0, delay: 5.5 }
+    );
+  }
+
   render() {
     return (
-      <div>
-        <p>Hello, GreenSock World!</p>
+      <div className="container">
+        <section className="intro" ref={this.intro}>
+          <p>A long time ago, in a galaxy far,<br /> far away....
+          </p>
+        </section>
       </div>
     );
   }
