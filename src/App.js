1008e14f571ec70dd0744e42d49bee9d519bd8cc
GiadaSimonetti <21024993+GiadaSimonetti@users.noreply.github.com>
2018-07-19 11:51:59 +0100

    Add a section with the crawl text + style for that

--- src/App.js
+++ src/App.js
@@ -30,8 +30,21 @@ class App extends Component {
           <p>A long time ago, in a galaxy far,<br /> far away...
           </p>
         </section>
+        
         <section className="logo" ref={this.logo}>
           <img src={logo} alt="Code Wars logo" />
+        </section>
+
+        <section className="crawl">
+          <div className="content" ref={this.content}>
+            <h1 className="title">Episode 7</h1>
+            <h2 className="subtitle">THE APP AWAKENS</h2>
+            <p>The Development Team Lead has vanished. In her absence, the sinister FUNCTIONAL BUG has risen from the ashes of the CI Tool and will not rest until the last developer has been destroyed.
+            </p>
+            <p>With the support of the QA TEAM, the Software Developer leads a brave RESISTANCE. He is desperate to find his Lead and gain her help in restoring peace and justice to the repository.
+            </p>
+            <p>The Developer has sent his most daring editor theme on a secret mission to the production branch, where an old ally has discovered a clue to the Lead’s whereabouts....</p>
+          </div>
         </section>
       </div>
     );
